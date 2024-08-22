346a96551feabbb0c8b35dab1f9d15d6c813d1f8
Katherine Rey <katherey97@gmail.com>
2024-08-21 21:27:40 -0500

    Nombre

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ const Home = () => {
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
           <Input handleSearch={handleSearch} setCountry={setCountry} />
           <h1 className="mb-8 md:mb-0 order-1 text-white py-2 rounded-xl italic font-bold">
-            Weather App.
+            Paises y Mapas.
           </h1>
         </div>
         {/* {data.current ? <div>{data.current.temp_f}</div> : null} */}
